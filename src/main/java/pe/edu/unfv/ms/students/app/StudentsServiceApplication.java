448d8003b2a3ab7b5f28b1d4689797ce8ab3dead
Eleazar Alfredo, ALFRIADEZ YRIARTE <ealfriadez@gmail.com>
2024-12-08 21:39:28 -0500

    first commit

---
+++ src/main/java/pe/edu/unfv/ms/students/app/StudentsServiceApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.unfv.ms.students.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentsServiceApplication.class, args);
+    }
+
+}
