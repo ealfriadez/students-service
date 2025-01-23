5e4907143284764946c5acb9b2cd30915b76adc1
Eleazar Alfredo, ALFRIADEZ YRIARTE <ealfriadez@gmail.com>
2025-01-23 09:52:28 -0500

    Finalizando correctamente el video Nro 9

--- src/main/java/pe/edu/unfv/ms/students/app/infrastructure/adapters/input/rest/GlobalControllerAdvice.java
+++ src/main/java/pe/edu/unfv/ms/students/app/infrastructure/adapters/input/rest/GlobalControllerAdvice.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import pe.edu.unfv.ms.students.app.domain.exceptions.StudentEmailAlreadyExistsException;
 import pe.edu.unfv.ms.students.app.domain.exceptions.StudentNotFoundException;
-import pe.edu.unfv.ms.students.app.infrastructure.adapters.input.rest.models.enums.ErrorType;
 import pe.edu.unfv.ms.students.app.infrastructure.adapters.input.rest.models.response.ErrorResponse;
 
 import java.time.LocalDate;
